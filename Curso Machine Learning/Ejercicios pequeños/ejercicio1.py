51095f6cd554e17bd16d57638c3e49cc817126f0
maalvrei <miguemorisco@gmail.com>
2023-01-19 13:07:53 +0100

    ml
    primera clase de ml

---
+++ Curso Machine Learning/Ejercicios pequeños/ejercicio1.py
@@ -0,0 +1,5 @@
+listaCiudades = ["Madrid" , "Nueva York" , "Tokio" , "Berlín" , "Pekin"]
+notificacion = "Madrid"
+listaCiudades.remove(notificacion)
+print(f'La ciudad eliminada ha sido {notificacion} y el motivo es la alta contaminación. \nAhora encabezan la lista {listaCiudades[0]} y {listaCiudades[1]}.')
+
---
+++ Curso Machine Learning/Ejercicios pequeños/ejercicio2.py
@@ -0,0 +1,5 @@
+
+import math
+n = -3.6
+x = math.floor(n)
+print(x)
